5f3c7d3fdc4d0a6d7db689df4c81824b3fb35701
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-23 21:46:13 +0530

    feat: complete typecasting

---
+++ 02_typecasting/main.py
@@ -0,0 +1,27 @@
+"""
+Typecasting= The process of converting a variable from one data type to another.
+             str(), int(), float(), bool()
+"""
+
+name= "Sid"
+age= 20
+gpa = 3.3
+is_student= True
+
+#gpa= int(gpa)
+#print(gpa)
+
+#age= float(age)
+#print(age)
+
+#is_student= str(is_student)
+# print(is_student)
+#print(type (is_student))
+
+#age= str(age)
+#age += "1"
+#print(age)     #201
+
+#name = bool(name)
+#print (name)   #Ture
+#print (name)   #False, if name= "" (empty string) 
