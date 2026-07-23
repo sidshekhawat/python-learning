b61f75e7536188a85b9a42ab3d5ad1116c4388af
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-23 21:20:51 +0530

    feat: complete variables

---
+++ 01_variables/main.py
@@ -0,0 +1,38 @@
+""" 
+Variable: A container for a value (string, integer, float, boolean)
+A variable behaves as if it was the value it contains.
+"""
+# Strings
+first_name= "Bro"
+food= "Pizza"
+email= "sid123@fake.com"
+
+#print(first_name)
+#print (f"Hello {first_name}.")
+#print (f"I Like {food}.")
+
+#Integers
+age= 25
+quantity= 3
+num_of_students= 30
+
+#print(f"You are {age} years old.")
+#print(f"You had {quantity} pizza's.")
+#print(f"Your Class has {num_of_students} Students.")
+
+#float
+price= 10.99
+distance= 5.5
+
+#print(f"The price is ${price}.")
+#print(f"You ran {distance}km.")
+
+#boolean
+
+is_student= True
+
+#print(f"Are you a student?: {is_student}.")
+"""if is_student:
+    print("You are a student.")
+else:
+    print("You are not a student.")"""
