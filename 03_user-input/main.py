7b3c58376c7f9daec26b00e79208260b6294146f
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-24 15:12:20 +0530

    feat: complete user-input

---
+++ 03_user-input/main.py
@@ -0,0 +1,19 @@
+"""
+input()= A function that prompts the user to enter data
+         Returns the entered d ata as a string
+"""
+
+"""name = input("What is your name?: ")
+age = input("What is your age? :")
+
+age = int(age)  #or we could do is typecast the user function age age = int(input("What is your age? :"))
+age = age + 1
+
+print (f"Hello {name}!") 
+print("It's my Birthday")
+print (f"I am {age} years old")"""
+
+length = float(input("Enter the Length:"))
+width = float(input("Enter the Width:"))
+area = length * width
+print(f"The area is {area}cm") 
