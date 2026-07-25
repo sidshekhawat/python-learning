6afedab3bb0967ecc7a4b54674f10931a2b58a03
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-25 12:12:50 +0530

    feat : complete conditional expressions

---
+++ 06_conditional-expressions/main.py
@@ -0,0 +1,21 @@
+"""
+conditional expressions = A one line shortcut for the if-else statement (ternary operator)
+                          Print or assign one of two values based on a condition
+                          X if condition else Y
+"""
+
+num = 6 
+a = 6
+b = 7
+age = 25
+user_role = "Guest"
+
+
+#print ("Positive" if num < 0 else "Negative")
+#print(a if a > b else b)
+#print (a if a < b else b)
+#status = 'Adult' if age >= 18 else 'Child'
+# print(status)
+
+#print("Full Access" if user_role == "Admin" else "Limited Access")
+
