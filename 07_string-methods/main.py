fe470480c0d503667ad286ee20e77c5e2ec12abe
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-25 12:49:06 +0530

    feat : complete string methods

---
+++ 07_string-methods/main.py
@@ -0,0 +1,16 @@
+#name = input('Enter your full name:')
+#phone_number = input('Enter your phone number #:')
+ 
+#print(len(name))         #Return len(self).
+#print(name.find("i"))    #Return the lowest index in S where substring sub is found, Return -1 on failure.
+#print(name.rfind("s"))   #Return the highest index in S where substring sub is found, Return -1 on failure.
+#print(name.capitalize()) #Return a capitalized version of the string.
+#print(name.upper())      #Return a copy of the string converted to uppercase. 
+#print(name.lower())      #Return a copy of the string converted to lowercase.
+#print(name.isdigit())    #Return True if the string is a digit string, False otherwise.
+#print(name.isalpha())    #Return True if the string is an alphabetic string, False otherwise.
+
+#print(phone_number.count('-'))
+#print(phone_number.replace('-', ' '))    #Return a copy with all occurrences of substring old replaced by new.
+
+print(help(str))
