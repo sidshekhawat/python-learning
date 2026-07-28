f3b6d2ca819d452d5e8297dcee2ba57d51f7b6eb
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-28 21:10:04 +0530

    refractor : standarize lesson folder names

---
+++ 04_Augmented_assignment_operators/main.py
@@ -0,0 +1,33 @@
+cookies = 10
+chocolates = 5
+
+#cookies = cookies + 1 
+#cookies += 1
+#cookies = cookies - 1
+#cookies -= 1
+#cookies = cookies * 2
+#cookies *= 2
+#cookies = cookies / 2
+#cookies /= 2 
+#cookies = cookies // 2
+#cookies //= 2
+#cookies = cookies ** 2
+#cookies **= 2
+cookies = cookies & chocolates
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print (cookies)
