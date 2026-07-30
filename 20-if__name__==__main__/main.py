e1672eaff30ea17b30aeb76b9f60760576cf7429
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-30 12:46:42 +0530

    feat: complete if __name__==__main__

---
+++ 20-if__name__==__main__/main.py
@@ -0,0 +1,13 @@
+"""
+if __name__ == __main__ : (this script can be imported OR run standalone)
+                          Functions and classes in this module can be reused without the main block of code executing
+
+Good practice (code is modular,
+               helps readability,
+               leaves no global variables,
+               avoid unintented execution)
+
+    Example = library = import library for functionality
+              When running library directly, display a help page
+"""
+
