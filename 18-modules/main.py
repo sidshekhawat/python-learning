29cbe69b6a573cc8c313b1a580b0d484ef5e76da
Siddhant Singh <sidshekhawat16@gmail.com>
2026-07-30 11:32:50 +0530

    feat : complete modules

---
+++ 18-modules/main.py
@@ -0,0 +1,16 @@
+"""
+module = a file containing code you want to include in your program
+         use 'import' to include a module (built-in or your own)
+         useful to break up a large program and to reuse separate files
+"""
+
+#print(help("modules"))
+
+import math
+#print(math.pi)
+
+import math as m 
+#print(m.pi)
+
+from math import pi
+#print(pi)
