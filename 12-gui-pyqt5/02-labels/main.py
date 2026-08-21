0115b5a7d09ffeb0100bb01e9026a0eb60a75039
Siddhant Singh <sidshekhawat16@gmail.com>
2026-08-21 18:50:26 +0530

    feat: complete PyQt5 Labels

---
+++ 12-gui-pyqt5/02-labels/main.py
@@ -0,0 +1,45 @@
+#PyQt5 Labels
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5. QtCore import Qt
+
+
+
+class MainWindow (QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0, 0, 500, 500)
+
+        label = QLabel("Hello", self)
+        label.setFont(QFont ("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: #292929;"
+                            "background-color: #6fdcf7;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+
+        #label.setAlignment(Qt.AlignTop) # VERTICALLY TOP
+        #label.setAlignment(Qt.AlignBottom) # VERTICALLY BOTTOM
+        #label.setAlignment(Qt.AlignVCenter) # VERTICALLY CENTER
+
+        #label.setAlignment(Qt.AlignRight) # HORIZONTALLY RIGHT
+        #label.setAlignment(Qt.AlignLeft) # HORIZONTALLY CENTER
+        #label.setAlignment(Qt.AlignHCenter) # HORIZONTALLY CENTER
+
+        #label.setAlignment(Qt.AlignHCenter | Qt.AlignTop) # CENTER & TOP
+        #label.setAlignment(Qt.AlignCenter | Qt.AlignBottom) # CENTER & BOTTOM
+        #label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) # CENTER & CENTER
+        #label.setAlignment(Qt.AlignCenter) # CENTER & CENTER
+  
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
